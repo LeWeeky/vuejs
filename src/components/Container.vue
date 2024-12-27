fbcb4d96e027af4a337ea17032c296ebc792e0df
LeWeeky <leweeky@clappycrew.com>
2024-12-27 21:54:24 +0100

    feat:    css: légère amélioration de la mise en page

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-container class="rounded-md" style="border: 1px solid #CCC; margin-top: 1vh; margin-bottom: 1vh;">
+	<v-container class="rounded-lg elevation-1" style="border: 1px solid #CCC; margin-top: 1vh; margin-bottom: 1vh;">
 		<slot> </slot>
 	</v-container>
 </template>
